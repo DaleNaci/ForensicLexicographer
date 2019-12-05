ff9a999d19dead58711205598c3953f2c74e91f4
Mayaank Vadlamani <mayaankvad@gmail.com>
2019-12-05 18:45:10 -0500

    add stuff

---
+++ book.py
@@ -0,0 +1,43 @@
+import statistics
+import pprint
+import string
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def does_end_at_period(line_list):
+    word = line_list[-1].lower()
+    return word not in ["sir.", "ms.", "mrs.", "mr.", "st."]
+
+def is_chapter_heading(line):
+    if "CHAPTER" in line.upper():
+        return False
+
+
+class Book:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.sentence_lengths = []
+        self.word_lengths = []
+        self.dialogue_words_count = 0
+
+    
+    def parse(self):
+        with open(self.filename, 'r') as book:
+            # panic()
+            pass
+                
+
+
+    def serialize(self):
+        word_count = len(self.word_lengths)
+        return {
+            "filename": self.filename,
+            "median_sentence_length": statistics.median(self.sentence_lengths),
+            "median_word_lengt": statistics.median(self.word_lengths),
+            "dialogue_ratio": self.dialogue_words_count / word_count,
+        }
+
+    def print():
+        pp.pprint(self.serialize())
