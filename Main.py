e742b908bcad246f380635e342b2a8d9c1bea382
DaleNaci <dale.nash01@gmail.com>
2019-12-06 00:02:55 -0500

    Created working structure for CML calling. Separates Training and Checking, as per Mayaank's suggestion.

--- Main.py
+++ Main.py
@@ -1,11 +1,37 @@
 import sys
+import os
 
 def main():
-    file_name = ""
-    if len(sys.argv) == 1:
-        file_name = input("What is the name of the file? ")
+
+    usage = "usage: python3 Main.py\t[-t <folder>]\n\t\t\t[-c <file>]"
+
+    if len(sys.argv) != 3:
+        print()
+        print("unknown option: " + " ".join(sys.argv[1:]))
+        print(usage)
+        print()
+        exit()
+
+    arg1, arg2 = sys.argv[1:]
+
+    if arg1 == "-t":
+        if os.path.isdir(arg2):
+            # TRAINING DATA
+            pass
+        else:
+            print("Folder does not exist.")
+
+    elif arg1 == "-c":
+        if os.path.isfile(arg2):
+            # CHECKING DATA
+            pass
+        else:
+            print("File does not exist.")
+
     else:
-        file_name = sys.argv[1]
+        print(usage)
+        exit()
+
 
 
 if __name__ == "__main__":
